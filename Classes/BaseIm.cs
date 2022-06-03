e95792524abe73e9f1c5b862b142873c077c7040
Aleksey Prosvirnin <a.prosvirnin@elnavf.ru>
2022-06-03 15:41:01 +0400

    fix

---
+++ Classes/BaseIm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECL.Classes
+{
+    class BaseIm
+    {
+        public UInt16 command;
+        public UInt32 status;
+        public bool commandFault;
+
+		//statusSet:smartPack:=(status:=status);	
+		//objectTable:TableDescriptor;
+		//_logTwoCommands:AloneLogMessage;
+	
+		private UInt16 _command =0;
+		private bool _haveCommand;
+		private uint _cmd;
+		private uint _source;
+		private uint _error;
+		private bool _needResetCmd;
+		private bool _opcCommandsDisabled = false;
+
+		//_errorReset:TON:=(PT:=T#15S);
+		//_cmdReset:TON:=	(PT:=T#1S);
+
+		
+
+
+    }
+}
---
+++ Classes/SmartPack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECL.Classes
+{
+    class SmartPack
+    {
+        ref UInt32 status;
+        public UInt32 settedBits;
+        public string name;
+
+        
+
+        private void Reset() => settedBits = 0;
+        private void SetBit(UInt16 bitNum, bool value) => SetBits(bitNum, (uint)value);
+        
+
+    }
+}
